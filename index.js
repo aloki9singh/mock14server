cf369a730998c2ae586277a3ec80c025351609a8
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-03-04 13:34:13 +0530

    backed updated

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./config/db");
+const { userRouter } = require("./Routes/User.route");
+
+const { authenticate } = require("./Middleware/authenticate.middleware");
+const { router } = require("./routes/Bug.router");
+
+const app = express();
+require("dotenv").config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Bug App");
+});
+
+app.use("/api", userRouter);
+app.use(authenticate);
+app.use("/api",router)
+app.listen(process.env.PORT, async (req, res) => {
+  try {
+    await connection;
+    console.log("Server Connected to database");
+  } catch (err) {
+    console.log({ err: err });
+  }
+  console.log(`Server Running On Port ${process.env.PORT}`);
+});
